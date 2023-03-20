3fa6e86104fa1ae2c389262246ebc6dd5bd3a53a
amrita <amritajais10@gmail.com>
2023-03-20 21:14:35 +0530

    Created Login API

--- index.js
+++ index.js
@@ -10,7 +10,21 @@ app.use(cors());
 app.post("/register", async (req, resp) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   resp.send(result);
 });
 
+app.post("/login", async (req, resp) => {
+  if (req.body.email && req.body.password) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No user Found" });
+    }
+  } else {
+    resp.send({ result: "Please Enter Correct Details" });
+  }
+});
 app.listen(5000);
