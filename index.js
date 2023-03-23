1f01825fc8b12c3e48dd45ab11902c50ad4250e2
amrita <amritajais10@gmail.com>
2023-03-23 21:47:31 +0530

    Single Product List, Update API done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("./db/config");
 const cors = require("cors");
 const app = express();
 const User = require("./db/User");
+const Product = require("./db/Product");
 
 app.use(express.json());
 app.use(cors());
@@ -27,4 +28,40 @@ app.post("/login", async (req, resp) => {
     resp.send({ result: "Please Enter Correct Details" });
   }
 });
+
+app.post("/add-product", async (req, resp) => {
+  let product = new Product(req.body);
+  let result = await product.save();
+  resp.send(result);
+});
+
+app.get("/product-list", async (req, resp) => {
+  let product = await Product.find();
+  resp.send(product);
+});
+
+app.delete("/delete/:id", async (req, resp) => {
+  let result = await Product.deleteOne({ _id: req.params.id });
+  resp.send(result);
+});
+
+app.get("/product/:id", async (req, resp) => {
+  let result = await Product.findById({ _id: req.params.id });
+  if (result) {
+    resp.send(result);
+  } else {
+    resp.send({ result: "No data Found" });
+  }
+});
+
+app.put("/product/:id", async (req, resp) => {
+  let result = await Product.updateOne(
+    {
+      _id: req.params.id,
+    },
+    { $set: req.body }
+  );
+  resp.send(result);
+});
+
 app.listen(5000);
